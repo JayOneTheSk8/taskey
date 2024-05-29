e8ee8f1c9b4430c3664da97b2c9e9a8de8ca4a6f
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-29 14:58:30 -0400

    maintain session token in User table * default to random text as default since it would be uncouth to add a new mandatory field without default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_195616) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_181434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_195616) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "session_token", default: -> { "md5((random())::text)" }, null: false
+    t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
