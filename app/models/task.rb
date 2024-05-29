e8eac23b34ce132ccbf6ddc25fa3a7b585057466
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-29 09:51:51 -0400

    add methods to manage task completion

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,16 @@ class Task < ApplicationRecord
 
   validates :title, :description, presence: true
   validates :completed, inclusion: {in: [true, false], message: "must be true or false"}
+
+  def complete!
+    update!(completed: true) if incomplete?
+  end
+
+  def mark_incomplete!
+    update!(completed: false) unless incomplete?
+  end
+
+  def incomplete?
+    !completed?
+  end
 end
