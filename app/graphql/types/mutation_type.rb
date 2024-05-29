b8be3151c46378aee7d2e0ba630d6fbd839ff13b
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-29 13:57:53 -0400

    add login and register mutations * log user in with Global ID

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,10 +2,38 @@
 
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false, description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    field(
+      :login,
+      Types::UserType,
+      null: false,
+      description: "Logs a User in with a Signed Global ID"
+    ) do
+      argument :username, String, required: true
+      argument :password, String, required: true
+    end
+    def login(username:, password:)
+      user = User.find_by_credentials(username, password)
+      raise GraphQL::ExecutionError, "Invalid username or password" unless user
+
+      user.token = user.to_sgid(expires_in: 12.hours, for: :graphql)
+      user
+    end
+
+    field(
+      :register,
+      Types::UserType,
+      null: false,
+      description: "Creates a User then logs them in with a Signed Global ID"
+    ) do
+      argument :username, String, required: true
+      argument :password, String, required: true
+    end
+    def register(username:, password:)
+      user = User.new(username:, password:)
+      raise GraphQL::ExecutionError, user.errors.full_messages.join(", ") unless user.save
+
+      user.token = user.to_sgid(expires_in: 12.hours, for: :graphql)
+      user
     end
   end
 end
