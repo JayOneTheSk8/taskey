eb5d00ee33011ef38e2a6310575e78f24781a29e
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-29 13:02:51 -0400

    add helper methods for current user and authentication validation

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -5,5 +5,15 @@ module Types
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
+
+    private
+
+    def current_user
+      context[:current_user]
+    end
+
+    def verify_authentication!
+      raise GraphQL::ExecutionError, "Must be signed in!" if current_user.blank?
+    end
   end
 end
