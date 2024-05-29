ffe7b99bc8920e860e36e9e5d4fcc148266e0987
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-29 13:05:33 -0400

    create User and Type GraphQL types

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :username, String, null: false
+    field :pending_tasks, [TaskType], null: false
+    field :completed_tasks, [TaskType], null: false
+    field :token, String, description: "The current authorization token for the user; exists only in memory."
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
