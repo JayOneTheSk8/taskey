1871e777b89e4e9b221ad8e0ef3220188b3c3565
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-29 15:08:52 -0400

    add helper methods for updated session auth - session: to get the session from application controller - logged_in?: to check if current user exists without error - login!: add user's session token to Rails session - reset_session!: nullifies Rails session's token

--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -12,8 +12,26 @@ module Types
       context[:current_user]
     end
 
+    def session
+      context[:session]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
     def verify_authentication!
       raise GraphQL::ExecutionError, "Must be signed in!" if current_user.blank?
+    end
+
+    def login!(user)
+      session[:session_token] = user.reset_session_token!
+      user.token = session[:session_token]
+      user
+    end
+
+    def reset_session!
+      session[:session_token] = nil
     end
   end
 end
