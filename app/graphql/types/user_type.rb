19b95436bff10d5409e4b62c2f6c3921e136a6c3
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-31 15:08:29 -0400

    dump graphql schema * add more descriptions to UserType

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,10 +4,21 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :username, String, null: false
-    field :pending_tasks, [TaskType], null: false
-    field :completed_tasks, [TaskType], null: false
-    field :token, String, description: "The session_token for the user; only present on login."
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    field :token, String, description: "The session_token for the user; only present on login."
+    field(
+      :pending_tasks,
+      [TaskType],
+      null: false,
+      description: "List of pending tasks sorted by ascending due date"
+    )
+    field(
+      :completed_tasks,
+      [TaskType],
+      null: false,
+      description: "List of completed tasks sorted by ID (and by proxy creation date)"
+    )
   end
 end
