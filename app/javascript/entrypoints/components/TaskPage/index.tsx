fc6911cf505562c824e3db8c82847bca0047d0aa
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-31 11:08:23 -0400

    add default TaskPage with user data * query for user and task data * log in user to ensure in state (this could probably go into the Root component but this is the home page technically)

--- app/javascript/entrypoints/components/TaskPage/index.tsx
+++ app/javascript/entrypoints/components/TaskPage/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { withStyles } from '@material-ui/core'
+import { useQuery } from '@apollo/client';
+
+import { AuthContext } from '../../authContext';
+import { ME_QUERY } from '../../queries/auth';
 
 const TaskPage = ({ classes }: {[key: string]: any}) => {
+  const context = useContext(AuthContext)
+
+  const { data, loading, error } = useQuery(ME_QUERY, {
+    onCompleted(data) {
+      const { id, username } = data.me;
+      context.login({ id, username })
+    }
+  });
+
+  if (error) return <div>ERROR</div>
+  if (loading) return <div>LOADING...</div>
+
   return (
     <>
-      <div>Tasks Here</div>
+      <div>{`${data.me.username}'s Tasks Here`}</div>
     </>
   )
 }
