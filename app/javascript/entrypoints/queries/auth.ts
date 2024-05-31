3eba3dc4f59674ddae16c73a77904fd3b2b61de0
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-31 10:44:27 -0400

    add register and login user mutations

--- app/javascript/entrypoints/queries/auth.ts
+++ app/javascript/entrypoints/queries/auth.ts
@@ -1,5 +1,25 @@
 import { gql } from '@apollo/client';
 
+export const REGISTER_USER =  gql`
+  mutation Register($username: String!, $password: String!) {
+    register(username: $username, password: $password) {
+      id
+      username
+      token
+    }
+  }
+`
+
+export const LOGIN_USER = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      username
+      token
+    }
+  }
+`
+
 export const LOGOUT_USER = gql`
   mutation Logout {
     logout
