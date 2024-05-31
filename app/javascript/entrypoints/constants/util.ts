6386fa9ff95c0f342a70a197ba04911624b4ae00
JayOneTheSk8 <misterjustincox@gmail.com>
2024-05-30 23:12:08 -0400

    add username and password field limits to constants

--- app/javascript/entrypoints/constants/util.ts
+++ app/javascript/entrypoints/constants/util.ts
@@ -2,6 +2,13 @@ const tokens = {
   SESSION_TOKEN: 'cust' // Current User Session Token
 }
 
+const limits = {
+  USERNAME_MAX_LIMIT: 50,
+  USERNAME_MIN_LIMIT: 4,
+  PASSWORD_MIN_LIMIT: 6,
+}
+
 export default {
+  limits,
   tokens,
 }
